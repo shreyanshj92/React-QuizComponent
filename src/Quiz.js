f7cfb594fdb1bed5f52d11faeae39db04e866371
Shreyansh Jain <shreyansh.jain@mindtree.com>
2022-08-30 16:25:23 +0530

    Completed Module 5

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,9 +9,13 @@ class Quiz extends Component {
     super(props);
     this.state = { quiz_position: 1 };
   }
+  showNextQuestion() {
+    this.setState({ quiz_position: this.state.quiz_position + 1 });
+  };
 
   render() {
-    const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
+    const isQuizEnd =
+      this.state.quiz_position - 1 === quizData.quiz_questions.length;
 
     return (
       <div>
@@ -22,6 +26,7 @@ class Quiz extends Component {
             quiz_question={
               quizData.quiz_questions[this.state.quiz_position - 1]
             }
+            showNextQuestionHandler={this.showNextQuestion.bind(this)}
           ></QuizQuestion>
         )}
       </div>
--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,22 +1,38 @@
-import React, { Component } from 'react'
-import QuizQuestionButton from './QuizQuestionButton'
+import React, { Component } from "react";
+import QuizQuestionButton from "./QuizQuestionButton";
 
 class QuizQuestion extends Component {
+    handleClick(buttonText) {
+        if (this.props.quiz_question.answer.includes(buttonText)) {
+          this.props.showNextQuestionHandler();
+        }
+      };
   render() {
+    
+
     return (
-        <main>
+      <main>
         <section>
           <p>{this.props.quiz_question.instruction_text}</p>
         </section>
         <section className="buttons">
-          <ul>
-            
-            <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}></QuizQuestionButton>
-          </ul>
+          {this.props.quiz_question.answer_options.map(
+            (answer_option, index) => {
+              return (
+                <ul>
+                  <QuizQuestionButton
+                    key={index}
+                    button_text={answer_option}
+                    clickHandler={this.handleClick.bind(this)}
+                  ></QuizQuestionButton>
+                </ul>
+              );
+            }
+          )}
         </section>
       </main>
-    )
+    );
   }
 }
 
-export default QuizQuestion
+export default QuizQuestion;
